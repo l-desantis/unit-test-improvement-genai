3e009c1d6c454424299e9a74f4467fc158209acf
Lorenzo De Santis <lorenzo.desantis@igt.com>
2024-03-15 15:01:25 +0100

    First Commit

---
+++ src/main/java/it/ringmaster/unittestimprovementgenai/UnitTestImprovementGenaiApplication.java
@@ -0,0 +1,13 @@
+package it.ringmaster.unittestimprovementgenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestImprovementGenaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestImprovementGenaiApplication.class, args);
+    }
+
+}
